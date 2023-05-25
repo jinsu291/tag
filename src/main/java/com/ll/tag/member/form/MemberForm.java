d524d4ac68549f04dc0ce923b509f22ecd28c277
jinsu291 <wlstn291@naver.com>
2023-05-25 14:30:21 +0900

    make MemberForm

---
+++ src/main/java/com/ll/tag/member/form/MemberForm.java
@@ -0,0 +1,16 @@
+package com.ll.tag.member.form;
+
+import lombok.Data;
+
+import javax.validation.constraints.NotEmpty;
+
+@Data
+public class MemberForm {
+    @NotEmpty
+    private String username;
+    @NotEmpty
+    private String password;
+    @NotEmpty
+    private String email;
+    private String nickname;
+}
