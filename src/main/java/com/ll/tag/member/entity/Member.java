f1f8f363e5c0cb33a905b7ac0081d7649eb5e4b3
jinsu291 <wlstn291@naver.com>
2023-05-25 14:26:41 +0900

    make BaseEntity, Member

---
+++ src/main/java/com/ll/tag/member/entity/Member.java
@@ -0,0 +1,44 @@
+package com.ll.tag.member.entity;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.ll.tag.base.entity.BaseEntity;
+import lombok.*;
+import lombok.experimental.SuperBuilder;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.util.StringUtils;
+
+import javax.persistence.Column;
+import javax.persistence.Convert;
+import javax.persistence.Entity;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Setter
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+@SuperBuilder
+@ToString(callSuper = true)
+public class Member extends BaseEntity {
+    @Column(unique = true)
+    private String username;
+    @JsonIgnore
+    private String password;
+    private String email;
+    private String nickname;
+    private boolean emailVerified;
+
+    public String getName() {
+        if (nickname != null) {
+            return nickname;
+        }
+
+        return username;
+    }
+
+    public Member(long id) {
+        super(id);
+    }
+}
